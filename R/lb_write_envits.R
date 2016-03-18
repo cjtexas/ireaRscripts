6ac94511d928066578bfeca90b9badb5164f4b47
lbusett <lbusett@gmail.com>
2016-03-18 14:33:04 +0100

    removed date_type keyword

--- R/lb_write_envits.R
+++ R/lb_write_envits.R
@@ -20,9 +20,7 @@ lb_write_envits = function(in_raster, in_dates, out_file) {
 
   writeRaster(in_raster, filename = out_file,overwrite = T, format = 'ENVI')
   bandnames = paste(basename(file_path_sans_ext(out_file)),in_dates, sep = '_')
-  if(date_type == "date") {
-    wl = as.numeric(lb_datetodoy(in_dates)+365*(year(in_dates)-min(year(in_dates))))
-    }
+  wl = as.numeric(lb_datetodoy(in_dates)+365*(year(in_dates)-min(year(in_dates))))
   hdrfile = paste0(file_path_sans_ext(out_file),'.hdr')
   write(paste("Band Names = {", paste (bandnames, collapse = ', '), "}", sep=""),file=hdrfile,append=TRUE)
   write(paste("wavelength = {", paste (wl, collapse = ', '), "}", sep=""),file=hdrfile,append=TRUE)
