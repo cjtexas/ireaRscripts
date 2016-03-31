e6e2a00b7350311810122d8d5cd752fab075689e
lbusett <lbusett@gmail.com>
2016-03-31 13:47:57 +0200

    updated help

--- R/lb_writeenvits.R
+++ R/lb_writeenvits.R
@@ -1,6 +1,6 @@
 
 #' lb_writeenvits
-#' @details Function to save an "R" multitemporal raster object to an ENVI multiband file (BSQ)
+#' @details Function to save an "R" multitemporal raster object (e.g., rasterStack) to an ENVI multiband file (BSQ)
 #' Bandnames and wavelengths of the output ENVI file are set on the basis of the acquisition dates .
 #' In paricular, wl takes the value of the acquisition doy, computed starting from 1st of January of the
 #' minimum year in the time serties
@@ -15,8 +15,19 @@
 #'
 #' @import raster tools
 #'
-#' @examples s
-lb_writeenvits = function(in_raster, in_dates, out_file) {
+#' @examples
+#'
+#' Create a raster stack from a list of single date raster
+#'
+#' in_files = file_path_sans_ext(list.files(in_folder, pattern = '*.tif$', full.names = T))   #List of files
+#' in_dates = as.Date(c('2000-01-01','2000-01-31',..... ))    # List of acquisition dates (MUST be same order as files)
+#' out_raster = 'd:/temp/out_multitemp
+#'
+#' # Create the ENVI raster file
+#' lb_writeenvits (in_raster = raster_in, in_dates = acq_dates, out_file = out_raster)
+#'
+
+lb_writeenvits = function(in_raster = in_raster, in_dates = in_dates, out_file = out_file) {
 
   writeRaster(in_raster, filename = out_file,overwrite = T, format = 'ENVI')
   bandnames = paste(basename(file_path_sans_ext(out_file)),in_dates, sep = '_')
