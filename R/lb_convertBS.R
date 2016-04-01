573453a6ba8444761424baf0d85c499438dc8bef
lbusett <lbusett@gmail.com>
2016-04-01 14:36:23 +0200

    aa

--- R/lb_convertBS.R
+++ R/lb_convertBS.R
@@ -1,3 +1,14 @@
+#' Title
+#'
+#' @param in_files
+#' @param in_dates
+#' @param out_rast
+#' @param shp_in
+#'
+#' @return
+#' @export
+#'
+
 lb_convertBS = function(in_files,in_dates, out_rast = out_rast, shp_in){
   out_rast_temp = paste0(out_rast,'.vrt')
   gdalbuildvrt(in_files, out_rast_temp, separate = T, overwrite = T, allow_projection_difference = T)
