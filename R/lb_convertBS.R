80f9858fea8c84022f15230ccdda60c8b41d9049
lbusett <lbusett@gmail.com>
2016-04-01 14:39:18 +0200

    s

--- R/lb_convertBS.R
+++ R/lb_convertBS.R
@@ -14,6 +14,6 @@ lb_convertBS = function(in_files,in_dates, out_rast = out_rast, shp_in){
   gdalbuildvrt(in_files, out_rast_temp, separate = T, overwrite = T, allow_projection_difference = T)
   in_rts = rts(brick(out_rast_temp), time = in_dates)     # create rasterstack timeseries object
   values(in_rts@raster) = as.integer(100*log10( values(in_rts@raster)))
-  lb_writeenvits2(in_raster = in_rts@raster, in_dates = in_dates, out_file = out_rast, dtype = 'INT2S')
+  lb_writeenvits(in_raster = in_rts@raster, in_dates = in_dates, out_file = out_rast, dtype = 'INT2S')
   gc()
 }
