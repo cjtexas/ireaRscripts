087d1e8b598b065c9a821baf419b2d7474d2a365
lbusett <lbusett@gmail.com>
2016-04-04 15:35:20 +0200

    bug in convertBS fixed

--- R/lb_convertBS.R
+++ R/lb_convertBS.R
@@ -13,7 +13,7 @@ lb_convertBS = function(in_files,in_dates, out_rast = out_rast, shp_in){
   out_rast_temp = paste0(out_rast,'.vrt')
   gdalbuildvrt(in_files, out_rast_temp, separate = T, overwrite = T, allow_projection_difference = T)
   in_rts = stack(out_rast_temp)     # create rasterstack timeseries object
-  r = calc(in_rts, fun = function(as.integer(100*log10(x)))
+  r = calc(in_rts, fun = function(x){as.integer(100*log10(x))})
   lb_writeenvits(in_raster = r, in_dates = in_dates, out_file = out_rast, dtype = 'INT2S')
   gc()
 }
