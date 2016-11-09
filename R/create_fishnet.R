e233bff60facc852896f623f814a0ed2161df1fd
lbusett <lbusett@gmail.com>
2016-11-09 14:55:47 +0100

    Added "create_fishnet" and "gdal_polygonizeR"

--- R/create_fishnet.R
+++ R/create_fishnet.R
@@ -1,85 +1,103 @@
 #' create_fishnet
-#' @description funtion to create a polygon fishnet of specified resoultion over a spatial object
-#'  Object can be a raster file, an extent, or a generic *SP object. If a raster is selected, the
-#'  grid is made so that upper left corner coincides with upper left corner of the raster.
-#' If both in_ext and in_rast are declared, the fishnet is cropped on raster extent.
-#' @param in_ext
-#' @param cellsize
-#' @param out_name_shp string(optional)
-#' @param expand
-#' @param in_rast
+#' @description funtion to create a raster and/or polygon fishnet of specified resolution over a
+#' a specified spatial extent. If both in_ext and crop_ext are declared, the fishnet is created
+#' according to in_ext (that is, the origin depends from in_ext), and then cropped on crop_ext (this
+#' allows for example to create a fishnet based on a given raster/resolution, and then to automatically
+#' crop it on a desired extgent without "moving" the corners of the cells ")
 #'
-#' @import rgdal
-#' @import raster
+#' @param in_ext        object of "Extent" class Extent on to which the grid has to be created
+#' @param crop_ext      object of "Extent" class Extent on to which the crfeated grid has to be cropped
+#' @param cellsize      numeric Dimensions of the desired cells
+#' @param in_proj       character proj4string projection of the desired fishnet
+#' @param out_raster    logical if TRUE, then a raster file is given as output. Defaults to TRUE
+#' @param out_shape     logical if TRUE, then a shape file is given as output. Defaults to FALSE
+#' @param out_rastfile  character (optional) name of the output raster file
+#' @param out_shapefile character (optional) name of the output shape file
+#' @param overw
+#'
+#' @importFrom sp GridTopology SpatialGridDataFrame CRS
+#' @importFrom raster setValues writeRaster crop brick
 #' @return
 #' @export
 #'
 #' @examples
+#' \dontrun{
+#'   # create a fishnet over an input raster file and save it as a shapefile
+#'   file_in <- "/somedir/somefile.tif"
+#'   inrast  <- raster(file_in)
+#'
+#'   ext_rast <- extent(inrast)
+#'   csize    <- res(in_mask)[1]
+#'
+#'   out_file <- "/somedir/somefile.shp"
+#'   create_fishnet(ext_rast, cellsize = csize, out_shape = TRUE, out_shapefile = out_file,
+#'          overw = TRUE, out_raster = FALSE)
+#'
+#' }
 
-create_fishnet <- function(in_ext = NULL, crop_ext = NULL, out_name_shp = NULL, cellsize = NULL, in_proj,
-                           out_format = 'raster', out_rastfile = NULL, out_shapefile = NULL, crop_onmask = FALSE) {
-
-  if (class(in_ext) != "Extent" | class(crop_ext) != c("Extent")) {
-    stop('error')
-  }
-
-  if (is.null(in_ext) ) {
-    stop('error')
-  }
-
-  if (!is.null(in_ext)) {
-    cc <- c(in_ext@xmin,in_ext@ymin)
-  }
-
-  # if (is.null(crop_ext)) {
-  #   in_ext   <-
-  #   cellsize <- res(in_rast)
-  #   cc       <- c(in_ext@xmin,in_ext@ymin) + (cs/2)  # cell offset   # move 1/2 pixel if necessary
-  # }
+create_fishnet <- function(in_ext, cellsize, in_proj, out_raster = TRUE, out_shape = FALSE,
+                           overw = FALSE, out_shapefile = NULL, crop_ext = NULL,
+                           out_rastfile = NULL) {
 
   if (is.null(cellsize)){
-      stop('error')
-    }
+    stop("cellsize not specified !")
+  }
 
+  if (class(in_ext) != "Extent"){
+    stop("in_ext must be an object of class: Extent")
+  }
+
+  if (!is.null(crop_ext) & class(crop_ext) != "Extent") {
+    stop('crop_ext must be an object of class: Extent')
+  }
+
+  if (is.null(cellsize)){
+    stop('error')
+  }
 
   cs <- c(cellsize,cellsize)  # cell size.
-
+  cc <- c(in_ext@xmin,in_ext@ymin) + (cs/2)   # corner of the grid. Since extent of a spatial object in R is defined
+  # by centroids, we need to move by half pixel
   # compute number of cells per direction
-  cd <- ceiling(c(((in_ext@xmax - in_ext@xmin)/cs[1]),((in_ext@ymax - in_ext@ymin)/cs[2]))) - 1
-
+  cd     <- ceiling(c(((in_ext@xmax - in_ext@xmin)/cs[1]),((in_ext@ymax - in_ext@ymin)/cs[2]))) - 1
   # construct grid topology
-  grd <- GridTopology(cellcentre.offset = cc, cellsize = cs, cells.dim = cd)   # Define grd characteristics
-
+  grd    <- GridTopology(cellcentre.offset = cc, cellsize = cs, cells.dim = cd)   # Define grd characteristics
   #transform to spatial grid
   sp_grd <- SpatialGridDataFrame(grd,
                                  data = data.frame(id = seq(1,(prod(cd)),1)),  # ids are numbers between 1 and ns*nl
                                  proj4string = CRS(in_proj))
 
+  # create RASTER ouput if required
+  if (out_raster) {
 
-  if (out_format == "raster") {
+    if (is.null(out_rastfile)) {out_rastfile <- tempfile("tempshp_", tempdir(), ".tif")}
+    message("Writing Polygon Grid to: ", out_shapefile, " - Please Wait !")
+
     out_raster <- raster(sp_grd)
     setValues(out_raster, sp_grd@data$id)
-    if (crop_onmask == TRUE) {
+
+    if (!is.null(crop_ext)) {
       out_raster <- crop(out_raster, crop_ext)
     }
-    if (!is.null(out_rastfile)) {
-      writeRaster(out_raster, out_rastfile, overwrite = TRUE)
-      return()
-    } else{
-      return(out_raster)
-    }
+
+    writeRaster(out_raster, out_rastfile, overwrite = TRUE)
+
   }
 
-  if (out_format == "shape") {
-    print('Creating Polygon Grid - Please Wait !')
-    if (crop_onmask == TRUE) {
-      sp_grd <- crop(sp_grd, crop_ext)
-    }
-    sp_polygons = as(sp_grd, "SpatialPolygonsDataFrame")
-    if (!is.null(out_shapefile)) {
-      writeshape(sp_polygons, out_name_shp, overw = TRUE) #save the shapefile
+  # create shapefile ouput if required
+  if (out_shape) {
+
+    if (is.null(out_shapefile)) {out_shapefile <- tempfile("tempshp_", tempdir(), ".shp")}
+    message("Writing Polygon Grid to: ", out_shapefile, " - Please Wait !")
+
+    if (!is.null(crop_ext)) {
+      sp_grd <- crop(brick(sp_grd), crop_ext)
     } else {
-      return(sp_polygons)
+      sp_grd <- raster(sp_grd)
     }
+
+    # Use routine "gdal_polygonizeR" to write the fishnet as a shapefile
+    gdal_polygonizeR(sp_grd, readpoly = F, outshape = out_shapefile, overwrite = overw)
+
   }
 }
